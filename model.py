dcd167f58377f783e0b3b31e5d0f668a9946550e
divyesh-rathod <divyeshrathod096@gmail.com>
2025-05-02 19:44:03 -0400

    building transformer

---
+++ model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class inputEmbedding(nn.Module):
+    def __init__(self, d_model:int, vocab_size:int ):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self,d_model:int ,seq_len:int,dropout:float):
+
+        super().__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+        self.seq_len = seq_len
+
+        pe = torch.zeros(seq_len,d_model)
+        postion = torch.arange(0,seq_len,dtype=torch.float).unsqueeze(1);
+        div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model))
+
+        pe[:,0::2] = torch.sin(postion/div_term)
+        pe[:,1::2] = torch.cos(postion/div_term)
+
+        pe = pe.unsqueeze(0);
+        self.register_buffer('pe' ,pe)
+
+    def forward(self, x):
+        x=x+(self.pe[:,:x.shape[1],:]).requires_grad_(False)
+        return(self.dropout(x))
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps:float = 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.bias = nn.Parameter(torch.zeros(1))
+        self.alpha = nn.Parameter(torch.ones(1))
+
+    def forward(self,x):
+        x_mean = x.mean(dim=-1,keepdim=True)
+        x_std = x.std(dim=-1,keepdim=True)
+        return self.alpha*(x-x_mean)/(x_std+self.eps)+self.bias
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model:int,d_ff:int,dropout:float):
+        super().__init__()
+
+        self.linear_1 = nn.Linear(d_model,d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff,d_model)
+
+    def forward(self,x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
